0d08dba1faeeef17b7e25a4a0641437ea096c3e5
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-11-10 22:59:46 +0300

    worker - docker

--- docker/worker/entrypoint.sh
+++ docker/worker/entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -u
 
-echo "Waiting for Kafka..."
-
-while ! nc -z broker 29092; do
-  sleep 2
-done
-
-echo "Kafka started"
-
 echo "Waiting for Postgresql..."
 
 while ! nc -z postgres 5432; do
--- worker/app/extract.py
+++ worker/app/extract.py
@@ -8,5 +8,6 @@ BATCH_SIZE = settings.BATCH_SIZE
 
 async def extract_payments() -> None:
     new_payments = await extract_new_payments()
-    logger.info(msg=new_payments)
-    await transform_payments(payments=new_payments)
+    if new_payments:
+        logger.info(msg=new_payments)
+        await transform_payments(payments=new_payments)
