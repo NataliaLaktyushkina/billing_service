66dc8062910a2b25e6526ff28246374f5fd2548c
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-11-12 00:25:32 +0300

    send to processing

--- processor/app/extract.py
+++ processor/app/extract.py
@@ -1,13 +1,11 @@
 from core.logger import logger
-from postgresql.db_settings.db_service import upload_payments
 from postgresql.db_settings.db_models import PaymentsStatus
-from postgresql.db_settings.db_service import change_payment_status
+from postgresql.db_settings.db_service import upload_payments
+from process import process_payments
 
 
 async def extract_payments() -> None:
     payments = await upload_payments(status=PaymentsStatus.to_process)
     if payments:
         logger.info(msg=payments)
-        for payment in payments:
-            await change_payment_status(payment=payment,
-                                        status=PaymentsStatus.in_processing)
+        await process_payments(payments=payments)
